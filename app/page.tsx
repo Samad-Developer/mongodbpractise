364f056b5101ce3dd198c5bffab3dfd5eb203918
Abdus samad <abdussamadkotha@gmail.com>
2024-08-23 22:43:10 +0500

    upload thing added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 // pages/index.tsx (or your desired homepage file)
+"use client"
 import prisma from '@/lib/prisma';
 import CreatePost from '@/components/CreatePost';
 import CreateImage from '@/components/CreateImage'
+import { UploadButton } from '@/src/utils/uploadthing';
 export default async function HomePage() {
   interface Product {
     id: string;
@@ -10,18 +12,31 @@ export default async function HomePage() {
     imageUrl?: string;
     createdAt: string;
   }
-  const posts = await prisma.post.findMany();
-  const products = await prisma.product.findMany();
+  // const posts = await prisma.post.findMany();
+  // const products = await prisma.product.findMany();
   return (
     <div>
      
       <div className='flex flex-col '>
       <h2>Create a New Post</h2>
-      <CreatePost/>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </main>
       <CreateImage/>
       <h1>Products</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
-        {products.map((product) => (
+        {/* {products.map((product) => (
           <div key={product.id} style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '8px', maxWidth: '200px' }}>
             <h2>{product.name}</h2>
             <p>Price: ${product.price.toFixed(2)}</p>
@@ -34,15 +49,15 @@ export default async function HomePage() {
             )}
             <p>Created At: {new Date(product.createdAt).toLocaleDateString()}</p>
           </div>
-        ))}
+        ))} */}
       </div>
       </div>
       <h1>Latest Posts</h1>
       
         <ul>
-          {posts.map((post) => (
+          {/* {posts.map((post) => (
             <li key={post.id}>{post.title}</li>
-          ))}
+          ))} */}
         </ul>
       
     </div>
